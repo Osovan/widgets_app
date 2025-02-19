7b00962c09f99b1e0d2d24dd17eb16d1e14b1805
Oscar Parada <oscarparada@gmail.com>
2025-02-19 13:39:56 +0100

    create Screens

---
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Animatedscreen extends StatelessWidget {
+  static const name = 'animacted_screen';
+  const Animatedscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Apptutorial extends StatelessWidget {
+  static const name = 'tutorial_screen';
+  const Apptutorial({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
